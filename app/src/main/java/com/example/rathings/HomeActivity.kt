aae1122fe70ddcbefb67badbb28a9a582576f249
lorenzo <lory.man95@gmail.com>
2019-04-11 16:14:00 +0200

    Added header navigation menu wich allow to swap sections made with fragments

--- app/src/main/java/com/example/rathings/HomeActivity.kt
+++ app/src/main/java/com/example/rathings/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.rathings
 
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,7 +12,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(),CardsFragment.OnFragmentInteractionListener,TabsFragment.OnFragmentInteractionListener,ProfileFragment.OnFragmentInteractionListener {
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
 
@@ -29,8 +30,32 @@ class HomeActivity : AppCompatActivity() {
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         /**END LOGOUT INIT*/
 
-        btn_logout.setOnClickListener { signOut() }
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> {
+                    val fragment = CardsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, "Cards").commit()
+                }
+                R.id.action_tabs -> {
+                    val fragment = TabsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, "Tabs").commit()
+                }
+                R.id.action_profile -> {
+                    val fragment = ProfileFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, "Profile").commit()
+                }
+                R.id.action_options -> {
+                    signOut()
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
     }
+
+    override fun onCardsFragmentInteraction(uri: Uri) {}
+    override fun onTabsFragmentInteraction(uri: Uri) {}
+    override fun onProfileFragmentInteraction(uri: Uri) {}
 
     fun signOut() {
         //Firebase Sign Out
