9513b4c78918318a65fad7ff95215da4ad9aa08d
lorenzo <lory.man95@gmail.com>
2019-07-18 09:42:59 +0200

    in-app notifications done

--- app/src/main/java/com/example/rathings/NotificationAdapter.kt
+++ app/src/main/java/com/example/rathings/NotificationAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.rathings
 
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.rathings.R
 import com.squareup.picasso.Picasso
 import java.util.*
@@ -21,6 +24,10 @@ class NotificationAdapter(private val mDataList: ArrayList<Notification>) : Recy
         val dateTime = java.util.Date(mDataList[position].timestamp.toLong() * 1000)
         holder.date.text = java.text.SimpleDateFormat("dd-MM-yyyy' - 'HH:mm", Locale.ITALY).format(dateTime)
         holder.text.text = "${mDataList[position].text}"
+
+        if(!mDataList[position].read){
+            holder.layout?.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        }
     }
 
     override fun getItemCount(): Int {
@@ -28,10 +35,12 @@ class NotificationAdapter(private val mDataList: ArrayList<Notification>) : Recy
     }
 
     inner class NotificationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var layout: LinearLayout? = null
         internal var date: TextView
         internal var text: TextView
 
         init {
+            layout = itemView.findViewById<View>(R.id.layout) as LinearLayout
             date = itemView.findViewById<View>(R.id.date) as TextView
             text = itemView.findViewById<View>(R.id.text) as TextView
         }
