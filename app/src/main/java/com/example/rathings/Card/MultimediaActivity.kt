04358990be02486aa200b05a3900d99e2c205f8b
luca150695@gmail.com <luca150695@gmail.com>
2019-07-19 10:07:39 +0200

    Begin video container in MultimediaActivity

--- app/src/main/java/com/example/rathings/Card/MultimediaActivity.kt
+++ app/src/main/java/com/example/rathings/Card/MultimediaActivity.kt
@@ -1,12 +1,16 @@
 package com.example.rathings.Card
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.MediaController
+import android.widget.VideoView
 import com.example.rathings.R
 import com.squareup.picasso.Picasso
 
@@ -32,23 +36,38 @@ class MultimediaActivity : AppCompatActivity() {
 
     fun setMultimedia(multimedia: ArrayList<String>) {
         var params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1F)
+        params.gravity = Gravity.CENTER_HORIZONTAL
         var padding = (20 * resources.displayMetrics.density + 0.5f).toInt()
 
-        var container_multimedia = findViewById<LinearLayout>(R.id.container_multimedia)
+        var container_images = findViewById<LinearLayout>(R.id.container_images)
+        var container_videos = findViewById<LinearLayout>(R.id.container_videos)
+
         for (i in multimedia.indices) {
-            var newLinearLayout = LinearLayout(applicationContext)
-            newLinearLayout.layoutParams = params
-            newLinearLayout.orientation = LinearLayout.VERTICAL
-            newLinearLayout.setPadding(0, 0, 0, padding)
+            if (multimedia[i].contains("image")) {
+                var newLinearLayout = LinearLayout(applicationContext)
+                newLinearLayout.layoutParams = params
+                newLinearLayout.orientation = LinearLayout.VERTICAL
+                newLinearLayout.setPadding(0, 0, 0, padding)
 
-            var imageView = ImageView(applicationContext)
+                var imageView = ImageView(applicationContext)
 
-            imageView.layoutParams = params
-            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-            Picasso.get().load(multimedia[i]).into(imageView)
-            newLinearLayout.addView(imageView)
+                imageView.layoutParams = params
+                imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                Picasso.get().load(multimedia[i]).into(imageView)
+                newLinearLayout.addView(imageView)
 
-            container_multimedia.addView(newLinearLayout)
+                container_images.addView(newLinearLayout)
+            } else {
+                var newLinearLayout = LinearLayout(applicationContext)
+                newLinearLayout.layoutParams = params
+                newLinearLayout.orientation = LinearLayout.VERTICAL
+                newLinearLayout.setPadding(0, 0, 0, padding)
+
+                // videoView.setVideoURI(Uri.parse(multimedia[i]))
+                // newLinearLayout.addView(videoView)
+
+                container_videos.addView(newLinearLayout)
+            }
         }
     }
 
